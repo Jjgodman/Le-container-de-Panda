45a65df08c245b46c2ff0668253ab8e46ad562c3
Marc-Antoine Giot <61468629+Jjgodman@users.noreply.github.com>
2023-02-04 14:52:44 +0100

    début du js

--- Front/js/index.js
+++ Front/js/index.js
@@ -4,17 +4,33 @@ async function main(){
     //affichage du menu
     await displyMenu()
     //affichage du contenu
-    displayContent()
+    for (let i = 0; i < 6; i++) {
+        await displayContent()
+    }
 }
 
 //fonction d'affichage du menu
 function displyMenu(){
-    console.log('displyMenu');
-    //récupération du menu
-    let menu = document.getElementById('menu')
-    //création du menu
-    menu.innerHTML += `
-    <footer>
-        <p>© 2019 Le container de Panda</p>
-    </footer>`
+    document.getElementById('menu').innerHTML += `
+    <nav id="menu">
+        <h1>Le container de Panda</h1>
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="about.html">A propos</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <footer>
+            <p>© 2019 Le container de Panda</p>
+        </footer>
+    </nav>`
+}
+
+//fonction d'affichage du contenu
+function displayContent(){
+    document.getElementById('grille_acceuil').innerHTML += `
+    <div class="blog">
+        <h2>Titre de l'article</h2>
+        <img src="../image/test.png" alt="image de l'article">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+    </div>`
 }
