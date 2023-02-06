1c47f446e475c5ec308a48a28c10ca21bbaeb125
Marc-Antoine Giot <61468629+Jjgodman@users.noreply.github.com>
2023-02-06 22:08:06 +0100

    ptite correct

--- Back/controllers/article.js
+++ Back/controllers/article.js
@@ -1,78 +1,56 @@
-//importation des bibliotheque nécessaire
 const Article = require('../models/article');
 
-//route pour récupérer tout les articles
-exports.getAllArticles = (req, res, next) => {
-    Article.find().then(
-        (Article) =>{
-            res.status(200).json(Article)
-        }
-    )
-    .catch(error => res.status(400).json({ error }));
+exports.getAllArticles = async (req, res) => {
+    try {
+      const articles = await Article.find();
+      res.status(200).json(articles);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
 };
 
-//route pour récupérer un article
-exports.getOneArticle = (req, res, next) => {
-    Article.findOne({
-        _id: req.params.id
-    }).then(
-        (article) => {
-            res.status(200).json(article)
-        }
-    )
-    .catch(error => res.status(404).json({ error }));
+exports.getOneArticle = async (req, res) => {
+    try {
+        const article = await Article.findOne({ _id: req.params.id });
+        res.status(200).json(article);
+    } catch (error) {
+        res.status(404).json({ error });
+    }
 };
 
-//route pour créer un article
-exports.createArticle = (req, res, next) => {
-    //récupération du FormData
-    const article = req.body;
-    //création d'un nouvel article
-    const art = new Article({
-        title: article.title,
-        content: article.content,
-        image: `../../Back/images/${req.file.filename}`,
-        intro: article.intro,
-        date: article.date
-    });
-    //sauvegarde de l'article dans la base de donnée
-    art.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Article enregistré !'
-            });
-        }
-    )
-    .catch(error => res.status(400).json({ error }));
-}
+exports.createArticle = async (req, res) => {
+    try {
+        const { title, content, intro, date} = req.body;
+        const image = `../../Back/images/${req.file.filename}`;
+        const newArticle = new Article({ title, content, image, intro, date });
+        await newArticle.save();
 
-//route pour modifier un article
-exports.modifyArticle = (req, res, next) => {
-    console.log('test');
-    const article = JSON.parse(req.body.article);
-    const art = new Article({
-        ...art,
-        imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
-    });
-    Article.updateOne({_id: req.params.id}, art).then(
-        () => {
-            res.status(201).json({
-                message: 'Article modifié !'
-            });
-        }
-    )
-    .catch(error => res.status(400).json({ error }));
-}
+        res.status(201).json({ message: 'Article enregistré !' });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
 
-//route pour supprimer un article
-exports.deleteArticle = (req, res, next) => {
-    Article.deleteOne({_id: req.params.id}).then(
-        () => {
-            res.status(200).json({
-                message: 'Article supprimé !'
-            });
-        }
-    )
-    .catch(error => res.status(400).json({ error }));
-}
+exports.modifyArticle = async (req, res) => {
+    try {
+        const parsedArticle = JSON.parse(req.body.article);
+        const { title, content, intro, date } = {...parsedArticle};
+        const imageUrl = `${req.protocol}://${req.get('host')}/image/${req.file.filename}`;
+        const updatedArticle = { title, content, imageUrl, intro, date};
 
+        await Article.updateOne({_id: req.params.id}, updatedArticle);
+
+        res.status(201).json({message: 'Article modifié !'});
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+
+exports.deleteArticle = async (req, res) => {
+    try {
+        await Article.deleteOne({_id: req.params.id});
+        res.status(200).json({ message: 'Article supprimé !' });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
